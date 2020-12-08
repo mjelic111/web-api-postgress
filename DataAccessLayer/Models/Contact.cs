663903b3068c69c13370474ce561308089ef6788
Marin <marin.jelic@gmail.com>
2020-12-08 11:42:02 +0100

    init

---
+++ DataAccessLayer/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    public class Contact: BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}

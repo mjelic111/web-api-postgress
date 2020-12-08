efade0bd5020b3978fa14edacba40783cdad0560
Marin <marin.jelic@gmail.com>
2020-12-08 12:00:47 +0100

    added postgres

---
+++ DataAccessLayer/Context/DatabasebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Context
+{
+    public class DatabasebContext: DbContext
+    {
+        public DatabasebContext(DbContextOptions<DatabasebContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}

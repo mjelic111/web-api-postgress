9e42834afae18efc4624784240227520e0910a30
Marin <marin.jelic@gmail.com>
2020-12-08 17:09:41 +0100

    init generic repository

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -37,6 +37,7 @@ namespace WebApi
 
             services.AddDbContext<DatabaseContext>(
                 options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

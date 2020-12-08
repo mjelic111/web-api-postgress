9fd085fefc6ff97b2bf82b6c592a19a09324c484
Marin <marin.jelic@gmail.com>
2020-12-08 20:31:57 +0100

    creating completed

--- DataAccessLayer/Repository/BaseRepository.cs
+++ DataAccessLayer/Repository/BaseRepository.cs
@@ -27,38 +27,58 @@ namespace DataAccessLayer.Repository
             return this;
         }
 
-        public async Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id, bool unitOfWork = false)
         {
-            var entity = context.Set<T>().SingleOrDefault(e => e.Id == id);
+            var entity = context.Set<T>().AsNoTracking().SingleOrDefault(e => e.Id == id);
             CheckIsEntityFound(entity, id);
             entity.Deleted = true;
-            await context.SaveChangesAsync();
-
+            if (!unitOfWork)
+            {
+                await context.SaveChangesAsync();
+            }
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await entitiesWithInclude.ToListAsync();
+            return await entitiesWithInclude.AsNoTracking().ToListAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            var entity = await entitiesWithInclude.SingleOrDefaultAsync(e => e.Id == id);
+            var entity = await entitiesWithInclude.AsNoTracking().SingleOrDefaultAsync(e => e.Id == id);
             CheckIsEntityFound(entity, id);
             return entity;
         }
 
-        public async Task InsertAsync(T entity)
+        public async Task<int> InsertAsync(T entity, bool unitOfWork = false)
         {
             CheckIsEntityNull(entity);
             context.Set<T>().Add(entity);
-            await context.SaveChangesAsync();
+            if (!unitOfWork)
+            {
+                return await context.SaveChangesAsync();
+            }
+            return 0;
         }
 
-        public async Task UpdateAsync(T entity)
+        public async Task UpdateAsync(T entity, bool unitOfWork = false)
         {
             CheckIsEntityNull(entity);
+            //var dbEntity = context.Set<T>().AsNoTracking().SingleOrDefault(e => e.Id == entity.Id);
+            //CheckIsEntityFound(entity, entity.Id);
+            //// dont override base entites
+            //entity.CreatedAt = dbEntity.CreatedAt;
+            //entity.Deleted = dbEntity.Deleted;
+            //update
             context.Entry(entity).State = EntityState.Modified;
+            if (!unitOfWork)
+            {
+                await context.SaveChangesAsync();
+            }
+        }
+
+            public async Task SaveChanges()
+        {
             await context.SaveChangesAsync();
         }
 
@@ -77,5 +97,6 @@ namespace DataAccessLayer.Repository
                 throw new EntityNotFoundException($"Entity with specified id: {id} not found");
             }
         }
+
     }
 }

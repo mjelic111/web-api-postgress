a4db0d4dd4e2fded1b2e34edc57ae6dde4fba25f
Marin <marin.jelic@gmail.com>
2020-12-08 17:29:51 +0100

    ongoing

--- DataAccessLayer/Models/IEntity.cs
+++ DataAccessLayer/Models/IEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,15 @@ namespace DataAccessLayer.Models
 {
     public interface IEntity
     {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Deleted { get; set; }
+    }
+
+    public class BaseEntity : IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool Deleted { get; set; }
